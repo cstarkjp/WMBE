0da83b069f6877702bf079ffc3325438ae1eb001
cstarkjp <cstarkjp@gmail.com>
2026-06-11 14:14:37 +0800

    Major refactoring of plot methods

--- notebooks/essentials.py
+++ notebooks/essentials.py
@@ -1,7 +1,12 @@
+import os
 import numpy as np
 import sympy as sy
 sy.init_printing(pretty_print=True,wrap_line=True)
 from sympy import Eq
 
 import wmbe
+from wmbe.file import create_directories, export_plots, export_plot
+from wmbe.solve1d import ErosionWeathering
 from wmbe.symbols import *
+from wmbe.theory import WeatheringMediatedErosion
+from wmbe.viz import Viz
